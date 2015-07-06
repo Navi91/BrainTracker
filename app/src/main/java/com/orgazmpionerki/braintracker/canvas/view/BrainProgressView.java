e883f7f64d34b6d9acd8150b4734f0a7a8fceefc
Dmitriy <navi91@inbox.ru>
2015-07-06 15:43:46 +0300

    Add new brain resource

--- app/src/main/java/com/orgazmpionerki/braintracker/canvas/view/BrainProgressView.java
+++ app/src/main/java/com/orgazmpionerki/braintracker/canvas/view/BrainProgressView.java
@@ -19,10 +19,25 @@ import com.braintracker.R;
 public class BrainProgressView extends View implements IValueView {
     private final Paint mPaint = new Paint();
     private Bitmap mBitmap;
-    private float mValue = 0f;
-    private final float mGreenBoundValue = 0.4f;
+    private BrainProgressState mPreviousState;
+    private BrainProgressState mCurrentState;
+    private final float mGreenBoundValue = 0.3f;
     private final float mRedBoundValue = 0f;
-    private int mProgressColor = Color.RED;
+
+    private class BrainProgressState {
+        float mValue = 0f;
+        int mColor = Color.GREEN;
+
+        BrainProgressState(float value, int color) {
+            mValue = value;
+            mColor = color;
+        }
+
+        void copy(BrainProgressState state) {
+            this.mValue = state.mValue;
+            this.mColor = state.mColor;
+        }
+    }
 
     public BrainProgressView(Context context) {
         super(context);
@@ -48,12 +63,16 @@ public class BrainProgressView extends View implements IValueView {
     private void init() {
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inMutable = true;
-        mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.brain_progress, options);
+        mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.test_brain, options);
+
+        mCurrentState = new BrainProgressState(0f, Color.WHITE);
+        mPreviousState = new BrainProgressState(0f, Color.WHITE);
     }
 
     @Override
     public void setValue(float value) {
-        mValue = value;
+        mPreviousState.copy(mCurrentState);
+
         updateProgressState(value);
 
         invalidate();
@@ -61,22 +80,57 @@ public class BrainProgressView extends View implements IValueView {
 
     @Override
     protected void onDraw(Canvas canvas) {
+        if (mPreviousState.mColor == mCurrentState.mColor) {
+            onDrawDiff(canvas);
+        } else {
+            onDrawNewColor(canvas);
+        }
+    }
+
+    private void drawBitmapPixel(Bitmap bitmap, int x, int y, float progress) {
+        if (y < progress) {
+            bitmap.setPixel(x, y, Color.WHITE);
+        } else {
+            bitmap.setPixel(x, y, mCurrentState.mColor);
+        }
+    }
+
+    private void onDrawDiff(Canvas canvas) {
+        int width = mBitmap.getWidth();
+        int height = mBitmap.getHeight();
+
+        float previousProgress = height * (1 - mPreviousState.mValue);
+        float currentProgress = height * (1 - mCurrentState.mValue);
+        int minProgress = (int) Math.min(previousProgress, currentProgress);
+        int maxProgress = (int) Math.max(previousProgress, currentProgress);
+
+        minProgress = minProgress > 0 ? minProgress : 0;
+        maxProgress = maxProgress > height - 1 ? height - 1 : maxProgress;
+
+        for (int x = 0; x < width; x++) {
+            for (int y = minProgress; y <= maxProgress; y++) {
+                if (isMaskPixelColor(mBitmap.getPixel(x, y))) {
+                    drawBitmapPixel(mBitmap, x, y, currentProgress);
+                }
+            }
+        }
+
+        canvas.drawBitmap(mBitmap, 0f, 0f, mPaint);
+    }
+
+    private void onDrawNewColor(Canvas canvas) {
         int width = mBitmap.getWidth();
         int height = mBitmap.getHeight();
 
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
                 if (isMaskPixelColor(mBitmap.getPixel(x, y))) {
-                    if (mProgressColor == Color.RED) {
-                        mBitmap.setPixel(x, y, mProgressColor);
+                    if (mCurrentState.mColor == Color.RED) {
+                        mBitmap.setPixel(x, y, mCurrentState.mColor);
                         continue;
                     }
 
-                    if (y < height * (1 - mValue)) {
-                        mBitmap.setPixel(x, y, Color.WHITE);
-                    } else {
-                        mBitmap.setPixel(x, y, mProgressColor);
-                    }
+                    drawBitmapPixel(mBitmap, x, y, height * (1 - mCurrentState.mValue));
                 }
             }
         }
@@ -89,12 +143,14 @@ public class BrainProgressView extends View implements IValueView {
     }
 
     private void updateProgressState(float value) {
+        mCurrentState.mValue = value;
+
         if (value > mGreenBoundValue) {
-            mProgressColor = Color.GREEN;
+            mCurrentState.mColor = Color.GREEN;
         } else if (value <= mGreenBoundValue && value >= mRedBoundValue) {
-            mProgressColor = Color.YELLOW;
+            mCurrentState.mColor = Color.YELLOW;
         } else {
-            mProgressColor = Color.RED;
+            mCurrentState.mColor = Color.RED;
         }
     }
 }
