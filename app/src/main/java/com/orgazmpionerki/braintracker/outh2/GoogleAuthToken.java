e9f431d6944a3bb677abea06d3a009b17435bb3d
Navi91 <navi91@inbox.ru>
2016-03-08 15:57:42 +0300

    Added new auth2 process

---
+++ app/src/main/java/com/orgazmpionerki/braintracker/outh2/GoogleAuthToken.java
@@ -0,0 +1,24 @@
+package com.orgazmpionerki.braintracker.outh2;
+
+import android.content.Context;
+import android.text.TextUtils;
+
+import com.orgazmpionerki.braintracker.util.Preferences;
+
+/**
+ * Created by Dmitriy on 08.03.2016.
+ */
+public class GoogleAuthToken {
+
+    public static boolean exist(Context context) {
+        return TextUtils.isEmpty(Preferences.getToken(context));
+    }
+
+    public static void setToken(Context context, String token) {
+        Preferences.setToken(context, token);
+    }
+
+    public static String getToken(Context context) {
+        return Preferences.getToken(context);
+    }
+}
---
+++ app/src/main/java/com/orgazmpionerki/braintracker/requests/TokenRequest.java
@@ -0,0 +1,52 @@
+package com.orgazmpionerki.braintracker.requests;
+
+import android.content.Context;
+
+import com.dkrasnov.util_android_lib.taskexecutor.request.HandleErrorRequestCallback;
+import com.dkrasnov.util_android_lib.taskexecutor.request.RequestTaskBase;
+import com.dkrasnov.util_android_lib.taskexecutor.request.RequestTaskCallback;
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.auth.GoogleAuthUtil;
+
+import java.io.IOException;
+
+/**
+ * Created by Dmitriy on 05.03.2016.
+ */
+public class TokenRequest extends RequestTaskBase<String> {
+    private Context context;
+    private String email;
+    private String scope;
+
+    public TokenRequest(Context context, String email, String scope) {
+        super();
+        init(context, email, scope);
+    }
+
+    public TokenRequest(Context context, String email, String scope, RequestTaskCallback<String> callback) {
+        super(callback);
+        init(context, email, scope);
+    }
+
+    public TokenRequest(Context context, String email, String scope, HandleErrorRequestCallback<String> callback) {
+        super(callback);
+        init(context, email, scope);
+    }
+
+    private void init(Context context, String email, String scope) {
+        this.context = context;
+        this.email = email;
+        this.scope = scope;
+    }
+
+    @Override
+    public String doRequest() {
+        try {
+            return GoogleAuthUtil.getToken(context, email, scope);
+        } catch (IOException | GoogleAuthException e) {
+            e.printStackTrace();
+            handleException(e);
+            return "";
+        }
+    }
+}
